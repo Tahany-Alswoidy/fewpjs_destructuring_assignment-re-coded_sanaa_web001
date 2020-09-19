28cd4500684a46646d13b4efadd69aec4c830fbb
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-19 05:33:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,12 +86,8 @@ for (var i = 0; i < ani.length; i++) {
     name[i]=ani[i];
   }
 }*/
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 const [black_and_white, black, pink]='cow sheep pig'.split(' ');
-// Arrays
-// 4. Use destructuring to assign appropriate variables using the color names.
 const [red, orange,yellow,green,blue,indigo,violet]=colors;
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 let [r, o,y,g,b,i,v]=colors;
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 let [r, o,y,g,b,indg,v]=colors;
